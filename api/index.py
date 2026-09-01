4810bf15d8f022653488551555c0cf8a70fc2d5d
bintangekop2005-tech <bintangekop2005@gmail.com>
2026-09-01 13:02:11 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,10 @@
 import os
-
 from dotenv import load_dotenv
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
-from google import genai
-from google.genai import types
-
 from pinecone import Pinecone
+import google.generativeai as genai
 
 
 # ============================================================
